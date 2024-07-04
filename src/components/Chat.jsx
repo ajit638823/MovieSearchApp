868f67a5609c48858220d5fc4ec6e1b9c5035f94
Atul <atulharsh273@gmail.com>
2024-07-04 22:20:41 +0530

    updated

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,15 +4,17 @@ import styles from "./Chat.module.css";
 import requests from "../request";
 import axios from "axios";
 import Loader from "./Loader";
+import MovieRow from "./MovieRow";
 function Chat() {
   const [query, setQuery] = useState("");
   const [disable, setDisable] = useState(false);
   const [genreId, setGenreId] = useState(null);
-  const [waiting, setWaiting] = useState(false);
+  const [loadingGenre, setLoadingGenre] = useState(false);
   const geminiKey = `AIzaSyCYReKvDdclir8B0RzAyS1YVj0O7LuGCDI`;
+  const movieFetchUrl = `https://api.themoviedb.org/3/discover/movie?api_key=1163e084f02e88f1718172d0524243e7&with_genres=`;
 
   async function fetchGenere() {
-    setWaiting(true);
+    setLoadingGenre(true);
     try {
       const res = await axios({
         method: "post",
@@ -22,19 +24,30 @@ function Chat() {
         },
       });
       const gen = res["data"]["candidates"][0]["content"]["parts"][0]["text"];
-      console.log(gen.length, gen);
+      setGenreId(gen.slice(0, gen.length - 2));
+      console.log(gen.slice(0, gen.length - 2));
     } catch (e) {
       console.log("error occured :", e);
     } finally {
-      setWaiting(false);
+      setLoadingGenre(false);
       setQuery("");
     }
   }
 
   return (
     <div className={styles.box}>
-      {waiting ? (
-        <Loader color={"white"} />
+      {loadingGenre ? (
+        <>
+          <Loader color={"white"} />
+          <h4>Analyzing the Prompt</h4>
+        </>
+      ) : genreId ? (
+        <>
+          <h4 style={{ position: "fixed" }}>Recommended</h4>
+          <div className={styles.results}>
+            <MovieRow heading={""} url={movieFetchUrl + genreId} />
+          </div>
+        </>
       ) : (
         <>
           <input
